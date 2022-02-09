1df5e5b6bdbb21e389aedd33d43137e9dd9493dd
@tpintopicotpro <tpintopicotpro@gmail.com>
2022-02-09 16:18:32 +0100

    get et set fonctionnel

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,62 @@
-const title = 'Hello TypeScript' as string;
 
-document.getElementById('content')!.innerHTML = `${title}`;
 
-console.log(title, stringYoYo(title));
+// constantes
+const url = 'https://web-help-request-api.herokuapp.com/users'
 
-/**
- * Convertit une lettre sur 2 en majuscule
- * @param title 
- */
-function stringYoYo(title: string): string
-{
-    const arr = title.split('');
 
-    const newArr = arr.map((letter, index) => {
-        if (index % 2 == 0) 
-        {
-            return letter.toUpperCase();
-        }
-        
-        return letter;
-        
-    });
+// variables 
 
-    return newArr.join('');
+
+
+
+// function 
+
+
+function addUser() {
+        let url = 'https://web-help-request-api.herokuapp.com/users'
+    fetch(url,{
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: "POST",
+    body: `username=Monsieur Mouche&password=1234`,
+}).then(response => {
+    return console.log(response)
+})
 }
+
+function getUsers() {
+    fetch(url).then(response => response.json().then((elm) => {
+        console.log(elm)
+}))}
+
+
+getUsers()
+
+function addTicket() {
+    let url = 'https://web-help-request-api.herokuapp.com/tickets'
+    fetch(url,{
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: "POST",
+    body: `subject=Nouvelle Methode&userId=14`,
+}).then(response => {
+    return console.log(response)
+})
+}
+
+function getTickets(element?: any) { 
+    let url ='https://web-help-request-api.herokuapp.com/tickets'
+
+    fetch(url).then(response => response.json().then((element) => console.log(element)))
+
+} 
+
+
+
+function disableTicket(element?: any) {
+   const url = `https://web-help-request-api.herokuapp.com/tickets`
+   fetch(url).then(response => response.json().then((element)  => console.log(element.data[0].done)))
+}
+
