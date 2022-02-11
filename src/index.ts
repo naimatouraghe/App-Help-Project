464e3aace3bce19843e97a5577a354f4facb2ce4
@tpintopicotpro <tpintopicotpro@gmail.com>
2022-02-11 11:21:57 +0100

    recup forEach user

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ let button = document.getElementById('butt')
 // function 
 
 
-button.addEventListener('click', function(e){
+button.addEventListener('click', function (e) {
     addUser()
 })
 
@@ -23,54 +23,58 @@ function addUser() {
     const username2: string = document.getElementById('username').value
     //@ts-ignore
     const password = document.getElementById('password').value
-    console.log(username2)  
-        let url = 'https://web-help-request-api.herokuapp.com/users'
-    fetch(url,{
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    method: "POST",
-    body: new URLSearchParams ({
-        'username':`${username2}`,
-        'password':`${password}`
-    }) ,
-}).then(response => {
-    return console.log(response)
-})
+    console.log(username2)
+    let url = 'https://web-help-request-api.herokuapp.com/users'
+    fetch(url, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        method: "POST",
+        body: new URLSearchParams({
+            'username': `${username2}`,
+            'password': `${password}`
+        }),
+    }).then(response => {
+        return console.log(response)
+    })
 }
 
 
 function getUsers() {
     fetch(url).then(response => response.json().then((elm) => {
-        console.log(elm)
-}))}
+        console.log(elm.data.forEach((element: { username: any }) => {
+            console.log(element)
+        }))
+    }))
+}
+
 
 getUsers()
 
 function addTicket() {
     let url = 'https://web-help-request-api.herokuapp.com/tickets'
-    fetch(url,{
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    method: "POST",
-    body: `subject=Nouvelle Methode&userId=14`,
-}).then(response => {
-    return console.log(response)
-})
+    fetch(url, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        method: "POST",
+        body: `subject=Nouvelle Methode&userId=14`,
+    }).then(response => {
+        return console.log(response)
+    })
 }
 
-function getTickets(element?: any) { 
-    let url ='https://web-help-request-api.herokuapp.com/tickets'
+function getTickets(element?: any) {
+    let url = 'https://web-help-request-api.herokuapp.com/tickets'
 
     fetch(url).then(response => response.json().then((element) => console.log(element)))
-} 
+}
 
 
 
 
 function disableTicket(element?: any) {
-   const url = `https://web-help-request-api.herokuapp.com/tickets`
-   fetch(url).then(response => response.json().then((element)  => console.log(element.data[0].done)))
+    const url = `https://web-help-request-api.herokuapp.com/tickets`
+    fetch(url).then(response => response.json().then((element) => console.log(element.data[0].done)))
 }
 
