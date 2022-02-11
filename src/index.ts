eb42869c0718c8101d5f9766b7096950e353a6f2
@tpintopicotpro <tpintopicotpro@gmail.com>
2022-02-11 13:41:31 +0100

    return data

--- src/index.ts
+++ src/index.ts
@@ -41,25 +41,23 @@ function addUser() {
 
 function getUsers() {
     fetch(url).then(response => response.json().then((elm) => {
-        console.log(elm.data.forEach((element: any) => {
+        elm.data.forEach((element: any) => {
             let ul = document.getElementById('list_name')
             let li = document.createElement('option')
             ul.appendChild(li)
             li.innerHTML = `<option class ="dropdown-item">${element.username}</option>`
             let butt_name = document.getElementById("butt_name")
+          
             //@ts-ignore
             // butt_name.addEventListener('submit', function (e) {
             //     //@ts-ignore
             //     username2.innerhtml = butt_name.value
             // })
-        }))
-    }))
+        })
+        return elm
+    })) 
+   
 }
-
-
-
-
-
 
 function addTicket() {
     let url = 'https://web-help-request-api.herokuapp.com/tickets'
@@ -78,6 +76,7 @@ function getTickets(element?: any) {
     let url = 'https://web-help-request-api.herokuapp.com/tickets'
 
     fetch(url).then(response => response.json().then((element) => console.log(element)))
+    return element
 }
 
 
