5a6d109d5beeccba8f2b1bea5e72613fb17818c0
@tpintopicotpro <tpintopicotpro@gmail.com>
2022-02-11 13:16:34 +0100

    option select

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,12 @@
 // constantes
 const url = 'https://web-help-request-api.herokuapp.com/users'
 //@ts-ignore
-
+const username2: string = document.getElementById('username').value
 
 // variables 
 let button = document.getElementById('butt')
 
-
-
 // function 
-
 
 button.addEventListener('click', function (e) {
     addUser()
@@ -20,7 +17,7 @@ button.addEventListener('click', function (e) {
 
 function addUser() {
     //@ts-ignore
-    const username2: string = document.getElementById('username').value
+
     //@ts-ignore
     const password = document.getElementById('password').value
     console.log(username2)
@@ -40,16 +37,29 @@ function addUser() {
 }
 
 
+
+
 function getUsers() {
     fetch(url).then(response => response.json().then((elm) => {
-        console.log(elm.data.forEach((element: { username: any }) => {
-            console.log(element)
+        console.log(elm.data.forEach((element: any) => {
+            let ul = document.getElementById('list_name')
+            let li = document.createElement('option')
+            ul.appendChild(li)
+            li.innerHTML = `<option class ="dropdown-item">${element.username}</option>`
+            let butt_name = document.getElementById("butt_name")
+            //@ts-ignore
+            butt_name.addEventListener('submit', function (e) {
+                //@ts-ignore
+                username2.innerhtml = butt_name.value
+            })
         }))
     }))
 }
-
-
 getUsers()
+
+
+
+
 
 function addTicket() {
     let url = 'https://web-help-request-api.herokuapp.com/tickets'
