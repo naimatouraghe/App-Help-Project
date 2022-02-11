f2c56f87493fabe22970d6a610117ee6d83355ec
@tpintopicotpro <tpintopicotpro@gmail.com>
2022-02-11 09:19:01 +0100

    input fonctionnel

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,42 @@
 
 // constantes
 const url = 'https://web-help-request-api.herokuapp.com/users'
+//@ts-ignore
 
 
 // variables 
-
+let button = document.getElementById('butt')
 
 
 
 // function 
+let username = "Capitaine Crochet"
+let password = "1234"
+
+button.addEventListener('click', function(e){
+    addUser()
+})
 
 
 function addUser() {
+    //@ts-ignore
+    const username2: string = document.getElementById('username').value
+    console.log(username2)  
         let url = 'https://web-help-request-api.herokuapp.com/users'
     fetch(url,{
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     },
     method: "POST",
-    body: `username=Monsieur Mouche&password=1234`,
+    body: new URLSearchParams ({
+        'username':`${username2}`,
+        'password':`${password}`
+    }) ,
 }).then(response => {
     return console.log(response)
 })
 }
+
 
 function getUsers() {
     fetch(url).then(response => response.json().then((elm) => {
